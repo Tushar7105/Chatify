af1c74ab923ce129d68e25974efb0cbb55665475
Tushar7105 <tushar070105@gmail.com>
2026-03-10 01:44:55 +0530

    add welcome email handler

---
+++ backend/src/lib/resend.js
@@ -0,0 +1,9 @@
+import resend from "resend";
+import "dotenv/config";
+
+const resendCliend = new resend(process.env.RESEND_API_KEY);
+
+export const sender = {
+    email : process.env.EMAIL_FROM,
+    name : process.env.EMAIL_FROM_NAME
+}
