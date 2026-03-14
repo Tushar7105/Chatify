d8bfb492b8d47854875699bfb492b74406f43857
Tushar7105 <tushar070105@gmail.com>
2026-03-15 01:06:11 +0530

    add logout logic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -88,3 +88,8 @@ export const login = async (req, res)=>{
         res.status(500).json({message : "internal server error"});
     }
 }
+
+export const logout = (_, res)=>{
+    res.cookie("jwt", "", {maxAge : 0});
+    res.status(200).json({message : "User logged out successfully"});
+}
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { login, signup } from "../controllers/auth.controller.js";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.get("/signup", signup);
 
 router.post("/login", login);
+
+router.post("/logout", logout);
+
 export default router;
-
-
-// key 
