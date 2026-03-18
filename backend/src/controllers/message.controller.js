82180e708b91bf7bc111851809a5c12b226213f9
Tushar7105 <tushar070105@gmail.com>
2026-03-19 01:03:02 +0530

    add getMessagesByUserId controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import Message from "../models/Message";
 import User from "../models/User";
 
 export const getContacts = async (req, res) => {
@@ -7,6 +8,26 @@ export const getContacts = async (req, res) => {
         return res.status(200).json(filteredUsers);
     }catch(error){
         console.log(error);
+        return res.status(500).json({message : "Internal server error"});
+    }
+}
+
+export const getMessagesByUserId = async (req, res)=>{
+    try{
+        const {id: receiverId} = req.params;
+        const loggedInUserId = req.user._id;
+
+        const messages = await Message.find({
+            $or : [
+                {senderId : loggedInUserId, receiverId : receiverId},
+                {senderId : receiverId, receiverId : loggedInUserId}
+            ]
+        });
+
+        res.status(200).json(messages);
+        
+    }catch(error){
+        console.log("error in getMessagesByUserId", error.message);
         return res.status(500).json({message : "Internal server error"});
     }
 }
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,14 +1,15 @@
 import express from "express";
-import { getContacts } from "../controllers/message.controller";
+import { getContacts, getMessagesByUserId } from "../controllers/message.controller";
 import { protectRoute } from "../middlewares/auth.middleware";
 
 const router = express.Router();
+router.use(protectRoute);
 
-router.get("/contacts", protectRoute, getContacts);
+router.get("/contacts", getContacts);
 
 // router.get("/chats", getChatPartners);
 
-// router.get("/messages/:id", getMessagesByUserId);
+router.get("/messages/:id", getMessagesByUserId);
 
 // router.post("/send/:id", sendMessage);
 
